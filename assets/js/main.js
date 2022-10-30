33775519e9add4cafd1084b79e796f8980cddac8
Rahaf Hosari <74103723+Rahafhosari@users.noreply.github.com>
2022-10-30 14:23:20 +0200

    modifications

--- assets/js/main.js
+++ assets/js/main.js
@@ -175,7 +175,7 @@
 
   // Testimonials carousel (uses the Owl Carousel library)
   $(".testimonials-carousel").owlCarousel({
-    autoplay: true,
+    // autoplay: true,
     dots: true,
     loop: true,
     items: 1
